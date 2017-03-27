ba00958b10a68fc28340dc1aac1902817ca20964
Angel Emilio Lopez Fernandez <appjak34@hotmail.com>
2017-03-27 12:28:19 -0600

    Initial Commit

---
+++ Simplex Revisado/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Simplex Revisado
+//
+//  Created by Angel Emilio Lopez Fernandez on 27/03/17.
+//  Copyright © 2017 Angel Emilio Lopez Fernandez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Simplex Revisado/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Simplex Revisado
+//
+//  Created by Angel Emilio Lopez Fernandez on 27/03/17.
+//  Copyright © 2017 Angel Emilio Lopez Fernandez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
