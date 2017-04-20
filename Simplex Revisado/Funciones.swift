2c7c663060236a62e704cdb103578f8a4b4369ff
Angel Emilio Lopez Fernandez <Appjak34@MacBook-Pro-de-Angel.local>
2017-04-20 00:47:28 -0500

    first commit

---
+++ Simplex Revisado/Funciones.swift
@@ -0,0 +1,9 @@
+//
+//  Funciones.swift
+//  Simplex Revisado
+//
+//  Created by Angel Emilio Lopez Fernandez on 27/03/17.
+//  Copyright © 2017 Angel Emilio Lopez Fernandez. All rights reserved.
+//
+
+import Foundation
